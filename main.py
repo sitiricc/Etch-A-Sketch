2f85e96ceba3ff32a6d7d2b9ce3360f2818f83e7
sitiricc <114097346+sitiricc@users.noreply.github.com>
2023-10-06 18:10:25 -0700

    Completed code for etch a sketch and added randomly picked color

--- main.py
+++ main.py
@@ -12,51 +12,57 @@ screen = Screen()
 
 
 def display_key_info():
+    # displays the keys for user to see
     etchy.penup()
-    etchy.goto(-301, -299)  # Adjust the coordinates as needed
+    etchy.goto(-301, -299) 
     etchy.write("'w': forward, 's':backward, 'a': counter-clockwise, 'd': clockwise, 'c': clear the screen.", align="left", font=("Arial", 9, "normal"))
     etchy.penup()
     etchy.goto(0, 0)  # Return to the starting position
     etchy.pendown()
 
 def random_color():
+    # picks random color for etch-a-sketch
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
     return "#{:02X}{:02X}{:02X}".format(r, g, b)  # Convert RGB to hexadecimal
 
-# Function to set the turtle's color and then move forward
+
 def move_forwards():
+    # Function to set the turtle's color and then move forward
     initial_color = random_color()
     etchy.color(initial_color)
     etchy.forward(10)
 
-# Function to set the turtle's color and then move backward
+
 def move_backwards():
+    # Function to set the turtle's color and then move backward
     initial_color = random_color()
     etchy.color(initial_color)
     etchy.backward(10)
 
 def counter_clockwise():
+    # Function to set the turtle's color and then move counter clockwise
     initial_color = random_color()
     etchy.color(initial_color)
     etchy.left(10)
     etchy.forward(10)
 
 def clockwise():
+    # Function to set the turtle's color and then move backward
     initial_color = random_color()
     etchy.color(initial_color)
     etchy.right(10)
     etchy.forward(10)
 
 def clear_screen():
+    # Function to clear screen
     etchy.clear()
     etchy.penup()
     etchy.home()
     etchy.pendown()
 
-# User answers
-
+# Key display and user answers
 display_key_info()
 screen.listen()
 screen.onkey(key="s", fun=move_backwards)
