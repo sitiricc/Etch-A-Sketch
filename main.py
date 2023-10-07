87673d92e1f71b02a575c4a0400b6f59fe87014a
sitiricc <114097346+sitiricc@users.noreply.github.com>
2023-10-06 18:09:20 -0700

    Changed code for etch-a-sketch

--- main.py
+++ main.py
@@ -1,29 +1,51 @@
 from turtle import *
-from art import *
+import random
 
-# w foward
+# w forward
 # s backward
 # a counter-clockwise
 # d clockwise
 # c clear drawing
 
+etchy = Turtle()
+screen = Screen()
 
-etchy= Turtle()
-screen= Screen()
 
+def display_key_info():
+    etchy.penup()
+    etchy.goto(-301, -299)  # Adjust the coordinates as needed
+    etchy.write("'w': forward, 's':backward, 'a': counter-clockwise, 'd': clockwise, 'c': clear the screen.", align="left", font=("Arial", 9, "normal"))
+    etchy.penup()
+    etchy.goto(0, 0)  # Return to the starting position
+    etchy.pendown()
 
-# functions to move mouse
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return "#{:02X}{:02X}{:02X}".format(r, g, b)  # Convert RGB to hexadecimal
+
+# Function to set the turtle's color and then move forward
 def move_forwards():
+    initial_color = random_color()
+    etchy.color(initial_color)
     etchy.forward(10)
 
+# Function to set the turtle's color and then move backward
 def move_backwards():
+    initial_color = random_color()
+    etchy.color(initial_color)
     etchy.backward(10)
 
 def counter_clockwise():
+    initial_color = random_color()
+    etchy.color(initial_color)
     etchy.left(10)
     etchy.forward(10)
 
 def clockwise():
+    initial_color = random_color()
+    etchy.color(initial_color)
     etchy.right(10)
     etchy.forward(10)
 
@@ -33,18 +55,15 @@ def clear_screen():
     etchy.home()
     etchy.pendown()
 
+# User answers
 
-
-
-# user answers
+display_key_info()
 screen.listen()
-screen.onkey(key= "s", fun= move_backwards)
-screen.onkey(key= "w", fun= move_forwards)
-screen.onkey(key= "a", fun= counter_clockwise)
-screen.onkey(key= "c", fun= clear_screen)
-screen.onkey(key= "d", fun= clockwise)
-
-
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="a", fun=counter_clockwise)
+screen.onkey(key="c", fun=clear_screen)
+screen.onkey(key="d", fun=clockwise)
 
 # Code to bring up screen
 screen.exitonclick()
